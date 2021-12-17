dc4048e22b5deda9b5a69ede0acbe55130a11af6
Cesar2006 <77751896+CSR-0@users.noreply.github.com>
2021-12-17 19:15:21 +0000

    One line if.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function solution(string) {
   let finalString = '';
   for(let i = 0; i < string.length; i++){
-    if(string[i] == string[i].toUpperCase()){
-      finalString += ' ';
-    }
+     finalString += string[i] == string[i].toUpperCase() ? ' ': '';
     finalString += string[i];
   }
   return finalString;
 }
 
-solution('')
+solution('camelCase');
 
